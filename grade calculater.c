ca07a64e8e5ff48ad68b4898491cf99ea5b29051
Lipiya-23 <libiyalibiya97@gmail.com>
2025-07-30 23:56:30 -0700

    Add files via upload

---
+++ grade calculater.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int marks[5],i,total=0;
+    float average;
+    char grade;
+    printf("Enter marks of 5 subjects(out of 100:\n");
+    for(i=0;i<5;i++){
+            printf("Subject%d:",i+1);
+    scanf("%d",&marks[i]);
+    //Validation
+    if(marks[i]<0||marks[i]>100){
+            printf("Invalid mark entered!Plese enter marks between 0 and 100.\n");
+    return 1;
+    }
+    total+=marks[i];
+    }
+    average=total/5.0;
+    //Grade calculation
+    if(average>=100)
+        grade='B';
+    else if(average>=50)
+        grade='C';
+        else if(average>=35)
+        grade='D';
+        else
+        grade='F';
+    printf("\n Total Marks:%d\n",total);
+    printf("Average:%.2f\n",average);
+    printf("Grade:%c\n",grade);
+    return 0;
+}
